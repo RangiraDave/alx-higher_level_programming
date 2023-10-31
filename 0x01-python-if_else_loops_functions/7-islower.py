43aac0adf3716776093063d59abd67a5024e5e26
RangiraDave <rangiradev666@gmail.com>
2023-10-31 18:51:55 +0200

    Mimmiced isupper() method

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     for i in range(ord('a'), ord('z') + 1):
-        if chr(i) == c:
+        if chr(i) == chr(ord(c)):
             return True
         else:
             return False
