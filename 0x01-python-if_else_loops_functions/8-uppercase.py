15d58aa03ca77f9bb2edb3708568d40a8d47acfe
RangiraDave <rangiradev666@gmail.com>
2023-10-31 20:02:11 +0200

    Function to change string chrs to UPPERcase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    i = 0
+    chr_list = list(str)
+    while i < len(str):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            chr_list[i] = chr(ord(str[i]) - 32)
+        i += 1
+    return "".join(chr_list)
