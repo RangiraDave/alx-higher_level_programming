1add4302e3912fe1e41a8ec42da476cee833eaf6
RangiraDave <rangiradev666@gmail.com>
2023-11-02 17:13:52 +0200

    Calculator with argv and imported functions from an other file

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
+        exit (1)
+
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        op = sys.argv[2]
+
+        if op == '+':
+            print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
+
+        if op == '-':
+            print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
+
+        if op == '*':
+            print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
+
+        if op == '/':
+            print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
