dc2dcc059d9e5e3e165247cdd29eb57f0952f6d8
RangiraDave <rangiradev666@gmail.com>
2023-11-02 17:15:58 +0200

    Removed white space before () of exit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
-        exit (1)
+        exit(1)
 
     else:
         a = int(sys.argv[1])
