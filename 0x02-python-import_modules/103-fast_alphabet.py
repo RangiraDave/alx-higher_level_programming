41ee3f1bf817e3d380fee5081e1578ee412a05ef
RangiraDave <rangiradev666@gmail.com>
2023-11-03 14:31:15 +0200

    used __name__...

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-import string
-print("{:s}".format(string.ascii_uppercase), sep='')
+if __name__ == "__main__":
+    import string
+    print("{:s}".format(string.ascii_uppercase), sep='')
