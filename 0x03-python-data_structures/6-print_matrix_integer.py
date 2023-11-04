f08ffc27e464de010ed5889aad4eda9a7833fa8c
RangiraDave <rangiradev666@gmail.com>
2023-11-04 17:10:01 +0200

    Met the expected output

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col_num in range(len(row)):
-            print("{:2d}".format(row[col_num]), end = '')
-        print()
+            print("{}".format(row[col_num]), end = '')
+            if col_num < len(row) - 1:
+                print("{}".format(" "), end = '')
+        print("{}".format(''))
