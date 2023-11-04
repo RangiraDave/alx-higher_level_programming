d452ad04b497f9041d6096141aa329df27a2ec7f
RangiraDave <rangiradev666@gmail.com>
2023-11-04 16:46:03 +0200

    Function to print a matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col_num in range(len(row)):
+            print("{:2d}".format(row[col_num]), end = '')
+        print("{:1s}".format("$"))
