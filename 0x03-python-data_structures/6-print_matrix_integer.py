6a79e8d5129ffc2e42e243d3ed0924d7013655ae
RangiraDave <rangiradev666@gmail.com>
2023-11-04 16:48:29 +0200

    Forgot to test using ./6-main.py | cat -e

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col_num in range(len(row)):
             print("{:2d}".format(row[col_num]), end = '')
-        print("{:1s}".format("$"))
+        print("{}".format(""))
