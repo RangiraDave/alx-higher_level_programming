17f4002bd459bfb8e2882e31b05c92823521bac6
RangiraDave <rangiradev666@gmail.com>
2023-11-07 08:41:03 +0200

    used a, b = b, a instead of a, b = 10, 89

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 a = 89
 b = 10
-a, b = 10, 89 if a and b else 'Usage: <a =...> and <b = ...>'
+a, b = b, a if a and b else 'Usage: <a =...> and <b = ...>'
 print("a={:d} - b={:d}".format(a, b))
