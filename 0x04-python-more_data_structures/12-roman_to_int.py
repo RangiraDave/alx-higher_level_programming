c021631a890dccff2e9cc97e80ce7ea769582ed0
RangiraDave <rangiradev666@gmail.com>
2023-11-08 20:15:15 +0200

    Roman to integer converter function

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    roman_val = {
+            'I': 1, 'V': 5, 'X': 10,
+            'L': 50, 'C': 100,
+            'D': 500, 'M': 1000
+            }
+
+    num, prev_value = 0, 0
+    for c in reversed(roman_string):
+        int_value = roman_val.get(c, 0)
+
+        if int_value >= prev_value:
+            num += int_value
+        else:
+            num -= int_value
+        prev_value = int_value
+
+    return num
