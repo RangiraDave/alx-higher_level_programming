1ca6160dfd1c9723c7d5515877514dc23a0151e1
RangiraDave <rangiradev666@gmail.com>
2023-11-08 15:27:07 +0200

    Function to double elements of a dictionary

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    return {i: a_dictionary[i] for i in a_dictionary}
+    new_dic = {i: a_dictionary[i] * 2 for i in a_dictionary}
+    return new_dic
