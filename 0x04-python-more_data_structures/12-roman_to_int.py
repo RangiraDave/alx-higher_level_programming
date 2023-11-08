c1994624cd513be355b320604bdd4d81340d2140
RangiraDave <rangiradev666@gmail.com>
2023-11-08 20:19:30 +0200

    Handeled some edge cases

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,6 +8,11 @@ def roman_to_int(roman_string):
             }
 
     num, prev_value = 0, 0
+    if roman_string == "":
+        return "None"
+    if not str(roman_string):
+        return 'None'
+
     for c in reversed(roman_string):
         int_value = roman_val.get(c, 0)
 
