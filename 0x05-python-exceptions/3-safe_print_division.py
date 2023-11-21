7363149c60a998f10fd572ebf8aa9412be8a0270
RangiraDave <rangiradev666@gmail.com>
2023-11-21 11:36:01 +0200

    Function to handle ZeroDivisionError

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,4 +8,4 @@ def safe_print_division(a, b):
         result = "None"
         return result
     finally:
-        print("Inside result: ", result)
+        print("Inside result: {}".format(result))
