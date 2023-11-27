a24555759cc70d3e36cb33119b283b6652e5c3dd
RangiraDave <rangiradev666@gmail.com>
2023-11-27 16:12:52 +0200

    Added function to print # with its test file

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This function takes in size"""
+
+
+def print_square(size):
+    """This function prints # size times"""
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+
+    for i in range(size):
+        print("#" * size)
