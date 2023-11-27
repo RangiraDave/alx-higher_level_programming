5ee65ba32db4ddbac282ed41c214a581dfd004fa
RangiraDave <rangiradev666@gmail.com>
2023-11-27 08:56:29 +0200

    My first commit on the TDD for add_integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,30 @@
+#/usr/bin/python3
+"""
+This function takes in two integers or floats
+and returns integer sum.
+"""
+def add_integer(a, b=98):
+    """
+    Function to add two integers
+
+    Args:
+        a (int or float): First integer or float.
+        b (int or float): Second integer or float. Defaults to 98.
+
+    Returns:
+        int: The addition of a and b.
+
+    Raises:
+        TypeError: If a or b is not an integer or float.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
+
+#if __name__ == "__main__":
+#    import doctest
+#    doctest.testfile("tests/0-add_integer.txt")
