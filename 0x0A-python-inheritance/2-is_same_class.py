d9ba392bd8fdc814c05f8b83bd0bbefd908d78bb
RangiraDave <rangiradev666@gmail.com>
2023-12-04 19:57:03 +0200

    is it its instance?

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,4 +6,5 @@ Function to check whether an object is an instance of a class provided.
 
 def is_same_class(obj, a_class):
     """Returns True if it is and False otherwise"""
-    return isinstance(obj, a_class)
+
+    return type(obj) is a_class
---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Does it inherit from class?"""
+
+
+def inherits_from(obj, a_class):
+    """This function takes odj and a_class"""
+
+    return type(obj) is not a_class
