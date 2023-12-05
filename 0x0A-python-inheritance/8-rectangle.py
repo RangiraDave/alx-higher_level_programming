983a0f9640470ed9ae429007d7d1da7526b8df18
RangiraDave <rangiradev666@gmail.com>
2023-12-05 09:24:21 +0200

    Added BaseGeometry.integer_validator(self, "width", width) and for height

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -11,4 +11,5 @@ class Rectangle(BaseGeometry):
 
         self.__width = width
         self.__height = height
-        BaseGeometry.integer_validator(self, width, height)
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
