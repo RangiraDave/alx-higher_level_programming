86bdfc60c7435dc814fa1a6fe84d7c5e87b5c03f
RangiraDave <rangiradev666@gmail.com>
2023-12-05 08:39:11 +0200

    Removed .format()

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,9 +13,9 @@ class BaseGeometry(object):
         """Function to validate value."""
 
         if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("<name> must be an integer")
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("<name> must be greater than 0")
 
         self.name = name
         self.value = value
