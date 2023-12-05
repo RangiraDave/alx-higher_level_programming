9efcd47f343507f4acc43670ed2a4f71d1e12d87
RangiraDave <rangiradev666@gmail.com>
2023-12-05 20:36:10 +0200

    Added Function to convert object to json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Lets turn it into json string shall we?"""
+import json
+
+
+def to_json_string(my_obj):
+    """This function returns json string of an object."""
+
+    return json.dumps(my_obj)
