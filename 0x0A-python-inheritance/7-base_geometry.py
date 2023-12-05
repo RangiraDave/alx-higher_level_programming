18dc22c76a6f598c3776d94213c9fee06ff16aff
RangiraDave <rangiradev666@gmail.com>
2023-12-05 08:35:02 +0200

    Added test cases for non int values

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,9 +13,9 @@ class BaseGeometry(object):
         """Function to validate value."""
 
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")
 
         self.name = name
         self.value = value
