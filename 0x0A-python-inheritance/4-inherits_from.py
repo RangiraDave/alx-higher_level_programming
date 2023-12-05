eab4351042e9beb068912b07abc917cdfaa144ed
RangiraDave <rangiradev666@gmail.com>
2023-12-05 08:01:51 +0200

    Added type(obj) is not a_class and issubclass((), )

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,4 +5,4 @@
 def inherits_from(obj, a_class):
     """This function takes odj and a_class"""
 
-    return type(obj) is not a_class
+    return type(obj) is not a_class and issubclass(type(obj), a_class)
