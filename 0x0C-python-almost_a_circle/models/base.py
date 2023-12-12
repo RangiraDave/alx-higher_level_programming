4c3cd5b31cc6d71f583f13aefde9d6a52440bfe9
RangiraDave <rangiradev666@gmail.com>
2023-12-12 07:35:27 +0200

    Handled "[]"

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,7 +22,7 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        if list_dictionaries is None or list_dictionaries is []:
+        if list_dictionaries is None or list_dictionaries is '[]':
             return []
         return json.dumps(list_dictionaries)
 
