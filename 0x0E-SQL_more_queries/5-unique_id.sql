658643b60b1bcb12ec27c5d3bea897f9e78abaa9
RangiraDave <rangiradev666@gmail.com>
2024-01-17 20:16:19 +0200

    Added table with unique id

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- Script to create table with uniqu eid.
-CREATE TABLE IF NOT EXISTS unique_id(id INT NOT NULL UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id(id INT NOT NULL UNIQUE DEFAULT(1), name VARCHAR(256));
