6d9b550f59143d9f49b5a705dc7d5311a2dc7f90
RangiraDave <rangiradev666@gmail.com>
2024-01-17 20:54:26 +0200

    asdded 8 taht displays data from different tables

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Script to display cities of California that can be found
 -- in the database.
 
-SELECT states.id, cities.name FROM states AND cities WHERE states.id = cities.state_id ORDER BY cities.id ASC;
+SELECT states.id, cities.name FROM cities WHERE states.id = cities.state_id ORDER BY cities.id ASC;
