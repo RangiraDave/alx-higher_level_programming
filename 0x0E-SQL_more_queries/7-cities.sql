3750c9cfe2ecb996bda8c33a8448668cdab3fc3a
RangiraDave <rangiradev666@gmail.com>
2024-01-17 20:34:29 +0200

    Script that creates my first foreign key in alx

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,6 @@
+-- Script to create database and put in cities table having
+-- a primary key "id" and foreign key from states.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY, state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
