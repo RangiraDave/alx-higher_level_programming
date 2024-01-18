8b02d09ebb01783b130fa80597086a13d9e65eb9
RangiraDave <rangiradev666@gmail.com>
2024-01-18 08:10:54 +0200

    Added Script to join citie with state

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- Script to display all cities in the database.
 
-SELECT cities.id AS i, cities.name, states.name FROM cities ORDER BY i ASC JOIN states;
+SELECT cities.id AS i, cities.name, states.name FROM cities ORDER BY i ASC NATURAL JOIN states;
