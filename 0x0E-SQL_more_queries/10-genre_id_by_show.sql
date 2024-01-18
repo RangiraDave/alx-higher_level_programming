39e7462248ec9d83006b6c7796825afd9a1aabce
RangiraDave <rangiradev666@gmail.com>
2024-01-18 08:58:45 +0200

    Playing with tv_show

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,4 @@
+-- Script to display the tv_shows.title and tv_show_genres.genre_id.
+
+SELECT tv_shows.tile, tv_show_genres.genre_id
+FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC AND tv_show_genres.genre_id ASC;
