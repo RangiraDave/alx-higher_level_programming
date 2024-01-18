3a2040408385586710cf9232a2a7cfd1003aa4f3
RangiraDave <rangiradev666@gmail.com>
2024-01-18 08:29:20 +0200

    ORDER BY must go after the join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,3 @@
 -- Script to display all cities in the database.
 
-SELECT cities.id, cities.name, states.name
-FROM cities
-NATURAL JOIN states
-ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities NATURAL JOIN states ORDER BY cities.id ASC;
