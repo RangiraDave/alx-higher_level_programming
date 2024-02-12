518cae617f0e9e95dc6d80e2cd55afe8c8da5d23
RangiraDave <rangiradev666@gmail.com>
2024-02-12 19:22:40 +0200

    started loop at 1

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@
 if (!Number(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < Number(process.argv[2]) + 1; i++) {
+  for (let i = 1; i < Number(process.argv[2]) + 1; i++) {
     console.log('C is fun');
   }
 }
