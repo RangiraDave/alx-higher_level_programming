1bac29c438f88133268db77175fc5a6d14e616dc
RangiraDave <rangiradev666@gmail.com>
2024-02-12 13:05:17 +0200

    Printing elements from arry

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Script to print from an array
+
+let arrayList = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i in arrayList) {
+  console.log(arrayList[i]);
+}
