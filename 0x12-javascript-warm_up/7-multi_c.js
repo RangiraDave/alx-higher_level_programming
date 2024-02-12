d88262d6b2f5aad3b3ab304eac36f2250c5c22b4
RangiraDave <rangiradev666@gmail.com>
2024-02-12 19:20:12 +0200

    printing message first argv times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// printing message multiple times acording to the first argv
+
+if (!Number(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < Number(process.argv[2]) + 1; i++) {
+    console.log('C is fun');
+  }
+}
