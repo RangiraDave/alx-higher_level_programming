1cb201de52159c528290cf61d9f8d0c041d8a110
RangiraDave <rangiradev666@gmail.com>
2024-02-13 17:30:55 +0200

    added print() and if s

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (Number(w) && Number(h)) {
+      if (Number(w) > 0 && Number(h) > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  }
+}
+
+module.exports = Rectangle;
