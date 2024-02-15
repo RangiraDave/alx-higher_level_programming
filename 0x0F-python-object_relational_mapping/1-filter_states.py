5679b11c8eb05284d2597e57bc08e3756133131d
RangiraDave <rangiradev666@gmail.com>
2024-02-15 21:24:23 +0200

    Selecting states where name starts with N and where name is equal to user prompt

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+import sys
+import MySQLdb
+
+def main(username, password, database):
+    """
+    Function to select all states from database hbtn_0e_0_usa
+    where name starts with N.
+    """
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./0-select_states.py <username> <password> <database>")
+        sys.exit(1)
+
+    username, password, database = sys.argv[1:]
+
+    main(username, password, database)
