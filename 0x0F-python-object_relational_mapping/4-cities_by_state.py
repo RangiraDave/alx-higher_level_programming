61ea93566de9336cde46085bc13599d622ca7604
RangiraDave <rangiradev666@gmail.com>
2024-02-18 14:59:48 +0200

    Used INNER JOIN and changed state_name to state_id

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -22,7 +22,7 @@ def main(username, password, database):
 
     cursor = db.cursor()
     cursor.execute("SELECT cities.id, cities.name, states.name \
-            FROM cities JOIN states ON cities.state_name = \
+            FROM cities INNER JOIN states ON cities.state_id = \
             states.id ORDER BY cities.id ASC")
     rows = cursor.fetchall()
     for row in rows:
