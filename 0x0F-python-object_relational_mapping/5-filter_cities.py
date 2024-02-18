fb540e507739433dfa6eeeae9fd0b127e4a5b2cf
RangiraDave <rangiradev666@gmail.com>
2024-02-18 16:11:12 +0200

    Added cities filter by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,25 +12,26 @@ def main(username, password, database, state):
     Function to implement so.
     """
 
-    db = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=username,
-            passwd=password,
-            db='hbtn_0e_4_usa'
-            )
+    try:
+        db = MySQLdb.connect(
+                host='localhost',
+                port=3306,
+                user=username,
+                passwd=password,
+                db='hbtn_0e_4_usa'
+                )
 
-    cursor = db.cursor()
-    cursor.execute("SELECT name FROM cities WHERE \
-            state_id = (SELECT id FROM state\
-            WHERE name = %s) \
-            ORDER BY cities.id ASC", (state))
+        cursor = db.cursor()
+        cursor.execute("SELECT name FROM cities WHERE \
+                state_id = (SELECT id FROM state\
+                WHERE name = %s) \
+                ORDER BY cities.id ASC", (state))
 
-    rows = cursor.fetchall()
-    city_names = [row[0] for row in rows]
-    print(', '.join(city_names))
+        rows = cursor.fetchall()
+        city_names = [row[0] for row in rows]
+        print(', '.join(city_names))
 
-    print()
+        print()
 
     except MySQLdb.Error as e:
         print("MySQLdb Error: {}".format(e))
