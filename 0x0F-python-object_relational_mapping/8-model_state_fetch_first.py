c00a99e85df31fd105b4bbab1b00161b11be359f
RangiraDave <rangiradev666@gmail.com>
2024-02-18 22:38:36 +0200

    Task 8 using main

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""this fetches in the database"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    """
+    Main function to handle the logic
+    """
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    host = "localhost"
+    connection = f'mysql+mysqldb://{username}:\
+        {password}@{host}:3306/{database}'
+
+    engine = create_engine(connection)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).order_by(State.id.asc()).first()
+
+    if state:
+        print(f"{state.id}: {state.name}")
+    else:
+        print("Nothing")
