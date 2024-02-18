bdfdc4c61242859aacb7a8dd105e0560800932dd
RangiraDave <rangiradev666@gmail.com>
2024-02-18 15:35:18 +0200

    Added cities filter by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,21 +13,21 @@ def main(username, password, database, state):
     """
 
     db = MySQLdb.connect(
-            host = 'localhost',
-            port = 3306,
-            user = username,
-            passwd = password,
-            db = 'hbtn_0e_4_usa'
+            host='localhost',
+            port=3306,
+            user=username,
+            passwd=password,
+            db='hbtn_0e_4_usa'
             )
 
     cursor = db.cursor()
     cursor.execute("SELECT cities.name FROM cities \
             INNER OUTER states ON cities.state_id = state.id \
-            WHERE state.name = '{}'
+            WHERE state.name = '{}' \
             ORDER BY citied.id ASC".format(state))
 
     rows = cursor.fetchall()
-    i=0
+    i = 0
     for cit in rows:
         i += 1
         if i != len(rows) - 2:
