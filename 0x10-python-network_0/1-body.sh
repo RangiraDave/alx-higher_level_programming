d3867d2e83bddded2ec197fc001758ee8b04a572
RangiraDave <rangiradev666@gmail.com>
2024-02-29 21:36:41 +0200

    added x permission

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script to display a body of the GET response (/^\r$/ identifies empty line btn header and body)
-curl -sL "$1" | awk 'NR==1,/^\r$/ {print $0; if($2 == 200) {body=1}} {if(body) print}'
+# Script to display body of the GET response (/^\r$/ identifies empty line btn header and body)
+curl -sL "$1" | awk 'NR==1,/^\r$/ {printf $0; if($2 == 200)}'
