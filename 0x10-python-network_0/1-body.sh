9b3c8b4caf41c7ec9847a1f238ffbb6066557c3e
RangiraDave <rangiradev666@gmail.com>
2024-02-29 20:33:39 +0200

    Used curl -siX with GET

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to display a body of the GET response (/^\r$/ identifies empty line btn header and body)
-curl -siX "GET" "$1" | awk 'NK==1,/^\r$/ {if ($2 == 200) {body=1}} {if(body) print}'
+curl -siX "GET" "$1" | awk 'NK==1,/^\r$/ {if ($1 == 200) {body=1}} {if(body) print}'
