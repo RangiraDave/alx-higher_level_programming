8dd0d3eb149b85914a9698976194c11984455ba1
RangiraDave <rangiradev666@gmail.com>
2024-02-29 21:56:50 +0200

    Added implementation of delete

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to implement delete
-curl -sX "DELETE" "$1" | awk 'NR==1,/^\r$/ {if($2 == 200); {printf $0}}'
+curl -sXL "DELETE" "$1" | awk 'NR==1,/^\r$/ {if($2 == 200); {printf $0}}'
