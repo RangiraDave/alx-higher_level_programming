582e3643c4374abf59f5dceba73b43245027eed3
RangiraDave <rangiradev666@gmail.com>
2024-02-29 20:07:19 +0200

    used only 3 lines-long script

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 # Script to read URL from the args
-# Send a request to it
-# And print its body size.
-
-if [ "$#" -ne 1 ]; then
-	echo "Usage: ./0-body_size.sh <URL/IP Adress>"
-	exit 1
-fi
-
-url="$1"
-size=$(curl -sI "$url" | grep -i 'content-length' | awk '{print $2}')
-
-echo "$size"
+$(curl -sI "$1" | grep -i 'content-length' | awk '{print $2}')
