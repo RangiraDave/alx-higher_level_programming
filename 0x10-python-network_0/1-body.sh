9f3cf8df18a72dfef45c1f0e9ee4e1f80b6d6d45
RangiraDave <rangiradev666@gmail.com>
2024-02-29 20:59:22 +0200

    Followed the redirect(3xx) using -L

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to display a body of the GET response (/^\r$/ identifies empty line btn header and body)
-curl -s "$1" | awk 'NR==1,/^\r$/ {if($2 == 200) {body=1}} {if(body) print}'
+curl -sL "$1" | awk 'NR==1,/^\r$/ {print "Line:", $0; if($2 == 200) {body=1}} {if(body) print}'
