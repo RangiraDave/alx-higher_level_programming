afbc7dd304b9d800ac1ee810901da61859cc9435
RangiraDave <rangiradev666@gmail.com>
2024-02-29 19:58:40 +0200

    used a shebang without usr

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -9,6 +9,6 @@ if [ "$#" -ne 2 ]; then
 fi
 
 url="$1"
-size=$(curl -sI "$url" | grep "content-length" | awk "{print $2}")
+size=$(curl -sI "$url" | grep -i "content-length" | awk "{print $2}")
 
 echo "$size"
