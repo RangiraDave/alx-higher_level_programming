4421e44294203cdc9d5c79fd10553e65acfa235e
RangiraDave <rangiradev666@gmail.com>
2024-03-01 08:38:53 +0200

    removed f on the printf

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to display all methods allowed by the server
-curl -sI "$1" | grep -i "Allow" | awk -F ': ' '{printf substr($0, index($0, $2))}'
+curl -sI "$1" | grep -i "Allow" | awk -F ': ' '{print substr($0, index($0, $2))}'
