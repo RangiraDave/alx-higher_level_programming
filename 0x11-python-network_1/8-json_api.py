970034768836e929a0b8e990c98554e3068cff44
RangiraDave <rangiradev666@gmail.com>
2024-03-01 21:45:01 +0200

    removed the function

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,27 +4,26 @@ Python script that send a POST request to a URL with a letter as
 a parameter and checks if body is properly JSON formatted.
 """
 
-from sys import argv
-import requests
+import sys
+import requests as rqs
 
 
-def json_check(letter):
+if __name__ == "__main__":
     """
     Function to handle logic.
     """
 
-    q = letter if letter 1 else ''
-
-    with requests.post('http://0.0.0.0:5000/search_user', data={'q': q}) as resp:
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) == 2:
+        q = {"q": sys.argv[1]}
+    else:
+        q = {"q": ""}
+    with rqs.post(url, data=q) as resp:
         try:
             page = resp.json()
             if page:
-                print("[{}] {}".format(page.get('id'), page.get('name')
+                print("[{}] {}".format(page.get("id"), page.get("name")))
             else:
                 print("No result")
         except ValueError:
             print("Not a valid JSON")
-
-
-if __name__ == "__main__":
-    json_check(argv[1])
