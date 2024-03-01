da208f2b94bb82ec9ae6a6418cc3e84e4c813833
RangiraDave <rangiradev666@gmail.com>
2024-03-01 21:06:20 +0200

    playing with JSON Payload

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Python script that send a POST request to a URL with a letter as
+a parameter and checks if body is properly JSON formatted.
+"""
+
+from sys import argv
+import requests
+import json
+
+q = argv[1] if len(argv) > 1 else ''
+
+r = requests.post('http://0.0.0.0:5000/search_user', json={'q': q})
+
+if not r.json():
+    print("No result" if q else "Not a valid JSON")
+else:
+    id_name = r.json().get('id'), r.json().get('name')
+    if None in id_name:
+        print("Not a valid JSON")
+    else:
+        print("[{}] {}".format(*id_name))
