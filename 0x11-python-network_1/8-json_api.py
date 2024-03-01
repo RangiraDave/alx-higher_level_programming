344d5078d4a16f948f55a73eb1a7ab93c0b4ad41
RangiraDave <rangiradev666@gmail.com>
2024-03-01 21:20:49 +0200

    Used a function

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,19 +8,24 @@ from sys import argv
 import requests
 
 
-q = argv[1] if len(argv) > 1 else ''
+def json_check(letter):
+    """
+    Function to handle logic.
+    """
 
-r = requests.post('http://0.0.0.0:5000/search_user', json={'q': q})
+    q = letter if letter 1 else ''
 
-if not r.json():
-    print("No result" if q else "Not a valid JSON")
-else:
-    id_name = r.json().get('id'), r.json().get('name')
-    if None in id_name:
-        print("Not a valid JSON")
+    r = requests.post('http://0.0.0.0:5000/search_user', json={'q': q})
+
+    if not r.json():
+        print("No result" if q else "Not a valid JSON")
     else:
-        print("[{}] {}".format(*id_name))
+        id_name = r.json().get('id'), r.json().get('name')
+        if None in id_name:
+            print("Not a valid JSON")
+        else:
+            print("[{}] {}".format(*id_name))
 
 
 if __name__ == "__main__":
-    pass
+    json_check(argv[1])
