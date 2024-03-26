2f7c486b2d98aed81bd59c7816ca8b750b2711dd
RangiraDave <rangiradev666@gmail.com>
2024-03-26 12:07:17 +0200

    added the script to read and print file contet

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// Script to read and display contend from a file
+
+const fs = require('fs');
+
+function readFileContent (filename) {
+  fs.readFile(filename, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(`Error reading the file: ${err}`);
+      return;
+    }
+    console.log(`${data}\n`);
+  });
+}
+
+const filename = process.argv[2];
+if (!filename) {
+  console.error('Provide a file name as an argument.');
+} else {
+  readFileContent(filename);
+}
