00d16838622d4817424ca209db065aa3ef6e3f0b
RangiraDave <rangiradev666@gmail.com>
2024-03-27 10:51:17 +0200

    Printing number of completed tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//Script that computes the number of tasks completed by user id.
+// Script that computes the number of tasks completed by user id.
 
 const request = require('request');
 const url = process.argv[2];
