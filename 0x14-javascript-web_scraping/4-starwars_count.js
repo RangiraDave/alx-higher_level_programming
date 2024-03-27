e06b99d7700f64a7d54eb836050f252d2067ec82
RangiraDave <rangiradev666@gmail.com>
2024-03-27 09:37:20 +0200

    Corrected typo

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -16,11 +16,10 @@ request(url, (err, response, body) => {
   if (err) {
     console.log(err);
   }
-  const movies = JSON.parse(body).results;
+  const results = JSON.parse(body).results;
   console.log(results.reduce((count, movie) => {
-      return movie.characters.find((character) => character.endsWith('/18/'))
-        ? count + 1
-        : count;
-    }, 0));
-  });
+    return movie.characters.find((character) => character.endsWith('/18/'))
+      ? count + 1
+      : count;
+  }, 0));
 });
